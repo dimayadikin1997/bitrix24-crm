0851270ec2e2a94742811fd9c803071004b18de6
Dima Yadikin <d.yadikin@alean.ru>
2025-08-16 12:00:54 +0300

    v1.0.1+2

---
+++ src/Service/Actions/ActionRegistry.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yadikin\Bitrix24\Crm\Service\Actions;
+
+/**
+ * Description of Actions
+ *
+ * @author dimay
+ */
+class Actions
+{
+    //put your code here
+}
